f56dda4f1e63afaaec982690d61ea4f78d952804
Andrzej1991 <andrzej.perski@hotmail.com>
2020-09-26 17:53:44 +0200

    added firebase integration

---
+++ script.js
@@ -0,0 +1,23 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBEwLpbIRohgkY9-_qW5SMcNzrbHz54OMk",
+  authDomain: "zhejtujpolityka.firebaseapp.com",
+  databaseURL: "https://zhejtujpolityka.firebaseio.com",
+  projectId: "zhejtujpolityka",
+  storageBucket: "zhejtujpolityka.appspot.com",
+  messagingSenderId: "615665011",
+  appId: "1:615665011:web:766ffa877b849aeecb8906",
+  measurementId: "G-SFKPTFTZJ0",
+};
+firebase.initializeApp(firebaseConfig);
+
+var ref = firebase.database().ref("DANE");
+ref.on(
+  "value",
+  function (snapshot) {
+    document.getElementsByClassName("fb-info")[0].innerHTML =
+      "Pomogliśmy już " + snapshot.val() + " osobom.";
+  },
+  function (error) {
+    console.log("Error: " + error.code);
+  }
+);
